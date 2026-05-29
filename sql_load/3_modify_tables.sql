5b50628071b541cf697403d792b7ce570a176959
ruhlanrzayev <contactruhlanrzayev@gmail.com>
2026-05-29 18:25:41 +0400

    created all folders and project files

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,9 @@
+SELECT
+    AVG(salary_year_avg) as average_year,
+    AVG(salary_hour_avg) as average_hour,
+    job_schedule_type
+FROM
+    job_postings_fact
+WHERE job_posted_date::DATE > '2023-06-01'
+GROUP BY
+    job_schedule_type;
